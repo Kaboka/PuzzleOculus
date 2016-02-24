1fc2439403fe56a49ad22ef4cf1e6bbfe9b905ea
Machtan <jako3047@gmail.com>
2016-02-24 13:16:48 +0100

    Add gitignore (and if the deleted 'library' folder doesn't mess everything up: mouse tracking on a test plane)

---
+++ Assets/Puzzlescript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzlescript : MonoBehaviour {
+
+	public Camera playerCamera;
+	public GameObject cursor;
+	protected GameObject lastCursor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.collider.gameObject.CompareTag("puzzle_screen")) {
+				Debug.Log("Puzzle Screen located!");
+				if (lastCursor == null) {
+					lastCursor = GameObject.Instantiate(cursor);
+				}
+
+				lastCursor.transform.position = (ray.origin + ray.direction * hit.distance);
+			}
+		}
+	}
+}
